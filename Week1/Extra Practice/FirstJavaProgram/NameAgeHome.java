7325f48e43c680de8444703c516617f938c4603a
Daniel Kennett <utahscs@gmail.com>
2022-02-07 15:53:48 -0500

    First Practice Commit

---
+++ Week1/Extra Practice/FirstJavaProgram/NameAgeHome.java
@@ -0,0 +1,7 @@
+public class NameAgeHome {
+    public static void main(String[] args) {
+        System.out.println("My Name is Daniel Kennett");
+        System.out.println("I am 32 years old");
+        System.out.println("My home town is West Jordan, UT");
+    }
+}
