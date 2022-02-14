b19a4bec5a4fa1ed4ded99e7558eb3cb61d03d45
Daniel Kennett <utahscs@gmail.com>
2022-02-14 16:48:23 -0500

    Orders and Items

---
+++ Week2/Extra Practice/OrdersItems/Order.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Order {
+    public String name;
+    public double total;
+    public boolean ready;
+    public ArrayList<Item> items;
+    
+    // for(int i = 0; i < items.size(); i++){
+    //     total += (items.get(i)).price;
+    // }
+}
